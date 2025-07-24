22fc4157eeb66ec35b0fb8e452d821cada8eeb7f
Anish Parekh <anishparekh02112001@gmail.com>
2025-07-24 16:19:39 +0530

    remove export ambiguity

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 // Export all components
 export * from './common';
-export * from './data-grid';
+// Re-export from data-grid with renamed FilterPopover to avoid collision
+export { FilterIcon, ClearAllFilters } from './data-grid';
+export { FilterPopover as DataGridFilterPopover } from './data-grid';
 export * from './layout';
 export * from './search';
